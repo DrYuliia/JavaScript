83003fe06b3765866e1981d23f29b326ba2c6300
Yuliia Drobenyuk <satire13@gmail.com>
2023-09-03 10:46:35 +0200

    Added home work 3

--- src/index.js
+++ src/index.js
@@ -1,84 +1,67 @@
 //Task 1
-
-const userInput = prompt('Enter Number');
-const parsedInput = parseInt(userInput);
-const result_a = parsedInput
-const result = (result_a % 3 === 0 && result_a % 5 === 0) ? 'Fizz Buzz' :
-               (result_a % 5 === 0) ? "Buzz" :
-               (result_a % 3 === 0) ? 'Fizz' : 'Oops it is not a nuber  or a number that is not divisible by 3 and 5';
-console.log(result);
-
-
-//Другий спосіб виконання першого завдання
-
 /*
 const userInput = prompt('Enter Number');
-const parsedInput = parseInt(userInput);
-const result_a = parsedInput
- if  (result_a % 3 === 0 && result_a % 5 === 0) {
-     console.log('FizzBuzz');
-}
-else if  (result_a % 3 === 0) {
-    console.log('Fizz');
- }
-else if (result_a % 5 === 0) {
-   console.log('Buzz');
-}
-else { 
-   console.log("Oops it is not a nuber  or a number that is not divisible by 3 and 5");
-}
+const number = parseInt(userInput);
+let counter = 0;
+
+for (let n = 1; n <= number; n++) {
+    if (number % n === 0)
+    { 
+        counter++;
+       
+    } 
+    
+} if (counter >= 3)
+        {
+            console.log(number+" Складене число");
+        }
+        else
+        {
+            console.log(number+" Просте число");
+} 
 */
 
 //Task 2
 /*
-const userInput = prompt('Enter the year, please.');
-const parsedInput = parseInt(userInput);
-const year = parsedInput;
-if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-{
-    console.log("Високосний рік");
+const userInput = prompt('Введіть число');
+const number = parseInt(userInput);
+let isPerfect = false; 
+
+for (let n = 1; n <= number; n++) {
+    let sum = 0;
+
+    for (let d = 1; d <= n / 2; d++) {
+        if (n % d === 0) {
+            sum += d;
+        }
+    }
+
+    if (sum === n) {
+        console.log(n + ' - це досконале число!');
+        isPerfect = true;
+    }
 }
 
-else { 
-   console.log("Не високосний рік");
+if (!isPerfect) {
+    console.log('Opps!');
 }
-*/
-//Другий спосіб виконання Task2
-/*
-const userInput = prompt('Enter the year, please.');
-const parsedInput = parseInt(userInput);
-const year = parsedInput;
-const result = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? "Високосний рік" : "Не високосний рік";
-console.log(result);
-*/
 
-
+*/
 //Task 3
-/*
-const userInput = prompt('Введіть ваш вік:');
-const parsedInput = parseInt(userInput);
-const num = parsedInput;
- 
 
-if (num==1 ||num==21 ||num==31 ||num==41 ||num==51 ||num==61 ||num==71 ||num==81 ||num==91 ||num==101) {
+const userInput = prompt('Enter Number');
+const number = parseInt(userInput);
+let counter ="";
+for (let i = 0; i <= number; i++) {
+    for (let space = 1; space <= number - i; space++)
+    {
+        counter += " ";
+            
+    }
+    for (let num = 1; num <= 2 * i - 1; num++)
+        {
+            counter += "*";
+        }
+        counter += "\n";
     
-    console.log("Вам " + num + " рік!");
-}
-else if (num==2||num==3 ||num==4 || num>=22&&num<=24 || num>=32&&num<=34 || num>=42&&num<=44  || num>=52&&num<=54 || num>=62&&num<=64 || num>=72&&num<=74 || num>=82&&num<=84 || num>=92&&num<=44 ||num==102)
-{ 
-     console.log("Вам " + num + " роки!");
-}
-    else if (num>=5 ||  num<=20 || num>=25&&num<=30 || num>=35 &&  num<=40 || num>=45 && num<=50  || num>=55 && num<=60 || num>=65 && num<=70 || num>75 && num<=80 || num>=85 && num<=90 || num==105)
-{ 
-      console.log("Вам " + num + " років!");
-}
-else if (num > 105) {
-    console.log("Хм..якийсь нереальний вік");
-}
-
-else {
-    alconsole.log("Opps. Ви ввели невірні дані!");
-   }     
-
-
-*/
+}console.log(counter);
