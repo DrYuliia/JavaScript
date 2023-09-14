30a8c5474a421f1b943a6e6f98ec7fe2d4b4cf39
Yuliia Drobenyuk <satire13@gmail.com>
2023-09-14 11:45:50 +0200

    Added home work 7

--- src/index.js
+++ src/index.js
@@ -1,44 +1,97 @@
-//Task1
-/*const getString = prompt('Enter string please:');
-let newString = '';
 
-function doubleLetter(str) {
-    for (let i = 0; i < str.length; i++) {
-        newString += str[i].repeat(2);
+//Task 1
+/*
+const originalArray = [1, 2, 3, 4, 5];
+let reversedArray = [];
+console.log(originalArray);
+
+function reverseArray(originalArray)
+{
+    for (i =originalArray.length - 1; i >= 0; i--)
+    {
+        reversedArray[reversedArray.length] = originalArray[i];    
     }
-    return newString;
+    return console.log(reversedArray);
 }
+reverseArray(originalArray);
 
-const result = doubleLetter(getString);
-console.log(result);
 */
 
 //Task 2
+
 /*
-const getString = prompt('Enter string please:');
-const userInput = prompt('Введіть число');
-const number = parseInt(userInput);
-const getSymbol = prompt('Enter symbol please:');
-const getTo = false;
+const array1 = [1, 2, 3, 4, 5,2];
+const array2 = [3, 4, 5, 6, 7, 10];
+let uniqueValuesArray = [];
 
+console.log('Array 1', array1);
+console.log('Array 2',array2);
 
-function padString(str, length, symbol, toLeft) {
-    if (str.length >= length) {
-        return str; 
+function uniqueValues(array1, array2) { 
+    for (let i = 0; i < array1.length; i++) {
+        let duplicate = false;
+        
+        for (let j = 0; j < uniqueValuesArray.length; j++) {
+            if (array1[i] === uniqueValuesArray[j]) {
+                duplicate = true;
+                break;
+            }
+        }
+        
+        if (!duplicate) {
+            uniqueValuesArray[uniqueValuesArray.length] = array1[i]; 
+        }
     }
-
-    const padding = symbol.repeat(length - str.length);
-
-    if (toLeft) {
-        return padding + str; 
-    } else {
-        return str + padding; 
+    for (let i = 0; i < array2.length; i++) {
+        let duplicate = false;
+        
+        for (let j = 0; j < uniqueValuesArray.length; j++) {
+            if (array2[i] === uniqueValuesArray[j]) {
+                duplicate = true;
+                break;
+            }
+        }
+        
+        if (!duplicate) {
+            uniqueValuesArray[uniqueValuesArray.length] = array2[i]; 
+        }
     }
+    return console.log('Unique array',uniqueValuesArray);
 }
 
-const result = padString(getString, number, getSymbol, getTo);
-console.log(result);
+uniqueValues(array1, array2);
 */
 
+//Task 3
+let totalGrades = 0;
+let averageGrade = 0;
+const students = [
+    {
+        name: "Alice",
+        age: 20,
+        grade: 4.5
+    },
+    {
+        name: "Bob",
+        age: 21,
+        grade: 3.9
+    },
+    {
+        name: "Charlie",
+        age: 19,
+        grade: 4.8
+    }
+];
 
-
+function calculateAverageGrade(students)
+{ 
+    for (let i = 0; i < students.length; i++)
+    {
+        
+            totalGrades += students[i].grade;
+        
+    }
+    averageGrade = totalGrades / students.length;
+    return console.log( averageGrade.toFixed(1));
+}
+calculateAverageGrade(students);
