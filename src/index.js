f9bdbf3bef752a7b551087877476a8e0fb57622f
Yuliia Drobenyuk <satire13@gmail.com>
2023-09-20 20:07:14 +0200

    Added home work 9

--- src/index.js
+++ src/index.js
@@ -1,80 +1,16 @@
 //Task 1
-/*
-const words = ["apple", "banaNA", "kiWi", "ORANGE"];
-const capitalizedWords = [];
-
-function capitalizeStrings(_arrayWords)
+const arrayNumbers = [1, 6, 9, 5, 2, 3, 12, 7, 8, 9, 10, 22, 4, 6, 8, 9, 10, 12, 14];
+const isSimple = function (number)
 {
-    console.log(_arrayWords);
+  let counter = 0;
+  for (let n = 1; n <= number; n++) {
+      if (number % n === 0)
+      {
+      counter++;
+     }
+  }
+  return counter <= 2; 
+};
+const arraySimpleNumbers = arrayNumbers.filter((number) => isSimple(number));
+console.log(arraySimpleNumbers);
 
-    for (let index = 0; index < _arrayWords.length; index++)
-        {
-            const element = _arrayWords[index].toLowerCase();
-            const firstLetter = element.charAt(0).toUpperCase();
-            const restOfWord = element.slice(1);
-            const capitalizedWord = firstLetter + restOfWord;
-            capitalizedWords.push(capitalizedWord);
-        }
-            console.log(capitalizedWords);
-            return capitalizedWords;
- } 
-capitalizeStrings(words);
-*/
-
-//Task 2
-/*
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-let commonElements =[];
-
-console.log('Array 1', array1);
-console.log('Array 2',array2);
-
-function findCommonElements(array1, array2)
-{
-    for (let index = 0; index < array1.length; index++)
-        {
-         const element = array1[index];
-
-        if (array2.includes(element) && !commonElements.includes(element)) {
-            commonElements.push(element);
-        }
-}
-    return commonElements;
-}
-
-console.log(findCommonElements(array1, array2));
-*/
-
-//Task 3
-const numbers = [1, 2, 3, 4, 5];
- function analyzeArray(numbers)
- {
-    let sum = 0;
-    let min = numbers[0];
-    let max = numbers[0];
-     
-     for (let i = 0; i < numbers.length; i++)
-     {
-         sum += numbers[i];
-        
-         if (numbers[i] < min)
-         {
-            min = numbers[i];
-        }
-         if (numbers[i] > max)
-         {
-            max = numbers[i];
-        }
-     } 
-   let  average = sum / (numbers.length - 1);
-           average = parseInt(average);
-
-    return {
-             sum: sum,
-             average: average,
-             min: min,
-             max: max
-           };
-}
-console.log(analyzeArray(numbers)); 
