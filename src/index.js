08d3922030293cf4caa29a5a6accaa3a7d52febd
Yuliia Drobenyuk <satire13@gmail.com>
2023-09-27 14:02:23 +0200

    Added home work11 task 1

--- src/index.js
+++ src/index.js
@@ -1,68 +1,16 @@
-// Task 1 
-
-const notifications = [
-  {
-    source: 'facebook',
-    text: ' Буданов розповів, як ЗСУ звільнятимуть Бахмут',
-    date: '25.06.23',
-  },
-  {
-    source: 'facebook',
-    text: 'ЗСУ прорвали оборону росіян у Вербовому',
-    date: '21.09.23',
-  },
-  {
-    source: 'twitter',
-    text: 'Iран брязкає зброєю',
-    date: '18.08.23',
-  },
-];
-
-
-
-function iteratorNotifications(notificationsArray) {
-  let index = 0;
-
-  return {
-    [Symbol.iterator]: function () {
-      return {
-        next: function () {
-          if (index < notificationsArray.length) {
-            return { value: notificationsArray[index++], done: false };
-          } else {
-            return { done: true };
-          }
-        },
-      };
-    },
-  };
-}
-
-function transformArrayToObject(notificationsArray)
-{
-  let notificationsObject = {};
-
-  const iterator = iteratorNotifications(notificationsArray);
-
-  for (const notification of iterator) {
-    const { source, text, date } = notification;
-
-    if (!notificationsObject[source])
-    {
-      notificationsObject[source] = [];
-    }
-
-    notificationsObject[source].push({ text, date });
+//Task1
+function logArguments(fn) {
+  return function (...args) {
+    console.log("Arguments:", args);
+    return fn(...args);
   }
-
-  return notificationsObject;
 }
 
+function add(a, b) {
+  return a + b;
+}
 
- 
+const loggedAdd = logArguments(add);
 
-
-
-const resultNotification = transformArrayToObject(notifications);
-console.log(resultNotification);
+const result = loggedAdd(1, 2);
 
