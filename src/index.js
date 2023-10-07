d6ee3660bfee2b5a4675fea456920f39810f1d8c
Yuliia Drobenyuk <satire13@gmail.com>
2023-10-07 17:05:13 +0200

    Add home work 14, task1

--- src/index.js
+++ src/index.js
@@ -1,103 +1,66 @@
-function Student(name, surname, age,course) {
-  this.name = name;
-  this.surname = surname;
-  this.age = new Date().getFullYear() -age;
-  this.marks = [];
-  this.attendance = [];
-  this.course = course;
-  this.totalLectures = this.course*20;// Припустимо,що в кожному курсі 20 лекцій
-}
-// Додавання оцінки 
-Student.prototype.addMark = function (mark)
-{
-    if (mark >= 0 && mark <= 10) {
-    this.marks.push(mark);
-    return true;
-  } else {
-    return false; // Повертаємо false, якщо оцінка недійсна
+class ToDoList {
+  constructor() {
+    this.notes = [];
   }
-  
-};
-//Середня оцінка
-Student.prototype.calculateAverageSuccess = function ()
-{
-  let averageSuccess = 0;
-  if (this.marks.length === 0)
-    {
-      return 0;
-    }
-  for (let i = 0; i < this.marks.length; i++)
-    {
-      averageSuccess += this.marks[i];
-    }
-    return averageSuccess / this.marks.length;
-};
 
-// Додавання відвідуваності 
-Student.prototype.addAttendance = function (attended)
-{
-  if (attended === true || attended === false)
-    {
-      this.attendance.push(attended);
-      return true;
+  addNotes(notes) {
+    if (notes === null) {
+      return "Error: Note is empty";
+    } else {
+      this.notes.push({ status: "Not done", notes });
     }
-  else
-    {
-      return false; 
-    }
-};
-
-Student.prototype.calculateAverageAttendance = function ()
-{
-  let attendedCount = 0;
-  if (this.attendance.length === 0)
-    {
-      return 0; 
   }
-  for (let i = 0; i < this.attendance.length; i++)
-    {
-       attendedCount+= this.attendance[i];
+
+  setStatus(index) {
+    if (index >= 0 && index < this.notes.length) {
+      this.notes[index].status = "Done";
     }
-    return ( attendedCount / this.attendance.length)*100;
+  }
+
+  deleteNote(index) {
+    if (index >= 0 && index < this.notes.length) {
+      this.notes.splice(index, 1);
+    }
+  }
+
+  editNote(index, newText) {
+    if (index >= 0 && index < this.notes.length) {
+      this.notes[index].notes = newText;
+    }
+  }
+
+  getNoteInfo(index) {
+    if (index >= 0 && index < this.notes.length) {
+      return this.notes[index];
+    } else {
+      return "Error: Invalid index";
+    }
+  }
+
+  getTotalNotes() {
+    return this.notes.length;
+  }
+
+  getRemainingNotes() {
+    return this.notes.filter((note) => note.status === "Not done").length;
+  }
+
+  getAllNotes() {
+    return this.notes;
+  }
 }
 
-// Отримати кількість пройдених занять
-Student.prototype.getLecturesAttended = function ()
-{
-  return this.attendance.filter(attended => attended === true).length;
-};
+const list = new ToDoList();
+list.addNotes("Study Spanish");
+list.addNotes("Go to the gym");
+list.addNotes("Change my work");
 
-// Змінити курс
-Student.prototype.changeCourse = function (newCourse) {
-  this.course = newCourse;
-  this.totalLectures = this.course * 20; // Оновлюємо кількість лекцій
-};
+console.log("Notes list:", list.getAllNotes());
 
+list.setStatus(2);
+list.deleteNote(1);
+list.editNote(0, "End running and start swimming");
 
-Student.prototype.getStudentInfo = function ()
-{
-  return `
-    Ім'я: ${this.name}
-    Прізвище: ${this.surname}
-    Вік: ${this.age}
-    Курс: ${this.course}
-    Оцінки: ${this.marks.join(', ')}
-    Відвідуваність: ${this.attendance.map(attended => attended ? 'відвідав' : 'не відвідав').join(', ')}
-    Середній бал: ${this.calculateAverageSuccess()}
-    Середня відвідуваність: ${this.calculateAverageAttendance()}%
-    Кількість пройдених занять: ${this.getLecturesAttended()} / ${this.totalLectures}
-  `;
-};
-
-
-const student = new Student('Switty', 'Chocolate', 2000, 1);
-
-console.log(student.getStudentInfo());
-
-student.addMark(8);
-student.addMark(9);
-student.addAttendance(true);
-student.addAttendance(true);
-student.changeCourse(2);
-
-console.log(student.getStudentInfo());
+console.log("Full note information 0:", list.getNoteInfo(0));
+console.log("All notes:", list.getTotalNotes());
+console.log("Not done notes:", list.getRemainingNotes());
