d78d6e356c1120d58eaad3f921aba1c79bc6b2f0
Yuliia Drobenyuk <satire13@gmail.com>
2023-10-16 12:29:56 +0200

    Added home work 15

--- src/index.js
+++ src/index.js
@@ -1,66 +1,23 @@
-class ToDoList {
-  constructor() {
-    this.notes = [];
-  }
-
-  addNotes(notes) {
-    if (notes === null) {
-      return "Error: Note is empty";
-    } else {
-      this.notes.push({ status: "Not done", notes });
-    }
-  }
-
-  setStatus(index) {
-    if (index >= 0 && index < this.notes.length) {
-      this.notes[index].status = "Done";
-    }
-  }
-
-  deleteNote(index) {
-    if (index >= 0 && index < this.notes.length) {
-      this.notes.splice(index, 1);
-    }
-  }
-
-  editNote(index, newText) {
-    if (index >= 0 && index < this.notes.length) {
-      this.notes[index].notes = newText;
-    }
-  }
-
-  getNoteInfo(index) {
-    if (index >= 0 && index < this.notes.length) {
-      return this.notes[index];
-    } else {
-      return "Error: Invalid index";
-    }
-  }
-
-  getTotalNotes() {
-    return this.notes.length;
-  }
-
-  getRemainingNotes() {
-    return this.notes.filter((note) => note.status === "Not done").length;
-  }
-
-  getAllNotes() {
-    return this.notes;
-  }
+function isPalindrome(n) {
+  return n.toString() === n.toString().split('').reverse().join('');
 }
 
-const list = new ToDoList();
-list.addNotes("Study Spanish");
-list.addNotes("Go to the gym");
-list.addNotes("Change my work");
+function reverseAndAdd(initialNum) {
+  let steps = 0;
+  let currentNumber = initialNum;
 
-console.log("Notes list:", list.getAllNotes());
+  while (steps < 1000) { 
+    if (isPalindrome(currentNumber)) {
+      return { result: currentNumber, steps };
+    } else {
+      currentNumber += parseInt(currentNumber.toString().split('').reverse().join(''));
+      steps++;
+    }
+  }
 
-list.setStatus(2);
-list.deleteNote(1);
-list.editNote(0, "End running and start swimming");
+  return { result: null, steps };
+}
 
-console.log("Full note information 0:", list.getNoteInfo(0));
-console.log("All notes:", list.getTotalNotes());
-console.log("Not done notes:", list.getRemainingNotes());
+
+const result = reverseAndAdd(96)
+console.log(result);
