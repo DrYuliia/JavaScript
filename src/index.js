9f2e58d896a720f639dd1824882b9a6698e5663d
Yuliia Drobenyuk <satire13@gmail.com>
2023-10-18 12:04:55 +0200

    Added home work 16 Task1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 function randomDelayPrint(message, index = 0) {
   if (index < message.length) {
     const letter = message[index];
-    const timer = Math.random() * 1000; // Випадкова затримка від 0 до 1 секунди
+    const timer = Math.random() * 1000; 
     setTimeout(function() {
       console.log(letter);
-      randomDelayPrint(message, index + 1); // Рекурсивний виклик для наступної літери
+      randomDelayPrint(message, index + 1);
     }, timer);
   }
 }
