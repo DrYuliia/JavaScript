37f53e6ef4c981ea3fa6e55d8d50c5ee4e4a6905
Yuliia Drobenyuk <satire13@gmail.com>
2023-10-25 11:30:36 +0200

    Added home work

--- src/index.js
+++ src/index.js
@@ -1,13 +1,69 @@
 //Task 1
-function randomDelayPrint(message, index = 0) {
-  if (index < message.length) {
-    const letter = message[index];
-    const timer = Math.random() * 1000; 
-    setTimeout(function() {
-      console.log(letter);
-      randomDelayPrint(message, index + 1);
-    }, timer);
-  }
+/*
+function sumArrayPromise(arr) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Array.isArray(arr)) {
+        const sum = arr.reduce((acc, current) => acc + current, 0);
+        resolve(sum);
+      } else {
+        reject("Input is not an array");
+      }
+    }, 3000);
+  });
 }
 
-randomDelayPrint("Hunter");
+sumArrayPromise([1, 2, 3, 4, 5])
+  .then(result => console.log(result))
+  .catch(error => console.error(error));
+*/
+//Task 2
+function concurrentPromises(promises, maxConcurrent) {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let currentIndex = 0;
+    let runningPromises = 0;
+
+    function runNextPromise() {
+      if (currentIndex >= promises.length) {
+        if (runningPromises === 0) {
+          resolve(results);
+        }
+        return;
+      }
+
+      while (runningPromises < maxConcurrent) {
+        const promise = promises[currentIndex];
+        const index = currentIndex;
+        currentIndex++;
+
+        promise()
+          .then(result => {
+            results[index] = result;
+          })
+          .catch(error => {
+            results[index] = error;
+          })
+          .finally(() => {
+            runningPromises--;
+            runNextPromise();
+          });
+
+        runningPromises++;
+      }
+    }
+
+    runNextPromise();
+  });
+}
+
+
+const promises = [
+  () => new Promise(resolve => setTimeout(() => resolve('Promise 1'), 1000)),
+  () => new Promise(resolve => setTimeout(() => resolve('Promise 2'), 500)),
+  () => new Promise(resolve => setTimeout(() => resolve('Promise 3'), 800))
+];
+
+concurrentPromises(promises, 2).then(result => {
+  console.log(result);
+});
